f95e07ef6cc7b0bb3b9b384e875492cbc139f146
KrTeaparty <hjk125ha@naver.com>
2020-02-15 00:10:58 +0900

    adding_background_image

--- src/ui/GUI.py
+++ src/ui/GUI.py
@@ -8,11 +8,17 @@ from PyQt5 import uic
  
 Practice_UI = uic.loadUiType('practice.ui')[0]
 
+
 class MainWindow(QMainWindow, Practice_UI):
     def __init__(self):
         super().__init__()
         self.setUI()
         self.InputBox.returnPressed.connect(self.commandline)
+        self.qPixmapVar = QPixmap()
+        self.qPixmapVar.load('lobto.png')
+        self.qPixmapVar = self.qPixmapVar.scaledToWidth(140)
+        self.label.setPixmap(self.qPixmapVar)
+        
 
     def setUI(self):
         self.setupUi(self)
@@ -21,7 +27,8 @@ class MainWindow(QMainWindow, Practice_UI):
         self.OutputBox.append(self.InputBox.text())
         self.InputBox.clear()
  
-app = QApplication(sys.argv)
-main_window = MainWindow()
-main_window.show()
-app.exec_()
+if __name__ == "__main__" :
+    app = QApplication(sys.argv)
+    main_window = MainWindow()
+    main_window.show()
+    sys.exit(app.exec_())
